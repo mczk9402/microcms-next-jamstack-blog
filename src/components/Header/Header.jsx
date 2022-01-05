6be360cd607c01558093354cd2a3e22682f2ffe9
望月祐汰 <mczk9402@gmail.com>
2022-01-05 13:27:45 +0900

    🐛 eslintエラー修正

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,6 +24,7 @@ export const Header = () => {
           setGlobalState({ type: 'SET_SITE_INFO', payload: { siteInfo: res } });
         });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
