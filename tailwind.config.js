0176de8329eb2fc8c65ea25c676613ed80ed574f
望月祐汰 <mczk9402@gmail.com>
2022-01-16 22:10:40 +0900

    🐛 tailwind 修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
@@ -7,15 +5,4 @@ module.exports = {
     extend: {},
   },
   important: true,
-  plugins: [
-    plugin(({ addUtilities }) => {
-      const newUtilities = {
-        '.bg-dot': {
-          backgroundImage:
-            'repeating-linear-gradient(0deg, rgba(242, 242, 242, 0.25) 0px 1px, transparent 1px 2px), repeating-linear-gradient(90deg, rgba(242, 242, 242, 0.25) 0px 1px, transparent 1px 2px)',
-        },
-      };
-      addUtilities(newUtilities, ['responsive', 'hover']);
-    }),
-  ],
 };
