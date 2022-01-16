3a0c10f95020c2cc78881733fb773bdbf5dc60fc
望月祐汰 <mczk9402@gmail.com>
2022-01-16 22:06:12 +0900

    🐛 tailwind修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       const newUtilities = {
         '.bg-dot': {
           backgroundImage:
-            'repeating-linear-gradient(0deg,rgba(242, 242, 242, 0.25) 0px 1px,transparent 1px 2px),repeating-linear-gradient(90deg, rgba(242, 242, 242, 0.25) 0px 1px, transparent 1px 2px)',
+            'repeating-linear-gradient(0deg, rgba(242, 242, 242, 0.25) 0px 1px, transparent 1px 2px), repeating-linear-gradient(90deg, rgba(242, 242, 242, 0.25) 0px 1px, transparent 1px 2px)',
         },
       };
       addUtilities(newUtilities, ['responsive', 'hover']);
