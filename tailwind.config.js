37be3420d04535e5162770f647f73aa37ffa76cf
望月祐汰 <mczk9402@gmail.com>
2022-01-16 21:48:52 +0900

    ✅ ユーティリティクラスbg-dotを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
@@ -5,4 +7,15 @@ module.exports = {
     extend: {},
   },
   important: true,
+  plugins: [
+    plugin(({ addUtilities }) => {
+      const newUtilities = {
+        '.bg-dot': {
+          backgroundImage:
+            'repeating-linear-gradient(0deg,rgba(242, 242, 242, 0.25) 0px 1px,transparent 1px 2px),repeating-linear-gradient(90deg, rgba(242, 242, 242, 0.25) 0px 1px, transparent 1px 2px)',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+  ],
 };
